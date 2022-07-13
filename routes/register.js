ec1c2e617ff47f3009fa3233c569d5ac332f88f6
John K <john.kellyfitness@gmail.com>
2022-07-13 17:49:14 -0400

    fixed register page

--- routes/register.js
+++ routes/register.js
@@ -9,34 +9,38 @@ router.post("/register", async function (req, res, done) {
   // retrieves name and email from user
   const name = req.body.name;
   const email = req.body.email;
-  if (name === null || email === null) {
+
+  if (name === null || email === null || req.body.password === null) {
     res.status(400).send("No data");
-  }
-  try {
-    // retrieves password and hashes it
-    const hashPassword = await bcrypt.hash(req.body.password, 10);
-    // creats new user model from retrieved data
-    const newNormalUser = {
-      firstName: name,
-      email: email,
-      password: hashPassword,
-    };
-    //looks through database to make sure the emial hasn't been used
-    let user = await User.findOne({ email: email });
-    if (user) {
-      done(null, user, { message: "User with that email already exist" });
-      res.render("register", { message: "User with that email already exist" });
-    } else {
-      //creates new user
-      user = await User.create(newNormalUser);
-      done(null, user);
-      res.status(200).send(user);
+  } else {
+    try {
+      // retrieves password and hashes it
+      const hashPassword = await bcrypt.hash(req.body.password, 10);
+      // creats new user model from retrieved data
+      const newNormalUser = {
+        firstName: name,
+        email: email,
+        password: hashPassword,
+      };
+      //looks through database to make sure the emial hasn't been used
+      let user = await User.findOne({ email: email });
+      if (user) {
+        done(null, user, { message: "User with that email already exist" });
+        res.render("register", {
+          message: "User with that email already exist",
+        });
+      } else {
+        //creates new user
+        user = await User.create(newNormalUser);
+        done(null, user);
+        res.status(200).send(user);
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(400).send({
+        message: err,
+      });
     }
-  } catch (err) {
-    console.error(err);
-    res.status(400).send({
-      message: err,
-    });
   }
 });
 
